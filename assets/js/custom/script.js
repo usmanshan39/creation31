ad5f1b5ee6a00fd3d2a249aab7932bb2e3ee3f30
usmanshan39 <70626124+usmanshan39@users.noreply.github.com>
2023-06-08 19:09:22 +0500

    two more pages

--- assets/js/custom/script.js
+++ assets/js/custom/script.js
@@ -1,2 +1,15 @@
-$(document).ready(function () {
-});
+$(document).ready(function() {
+    $("#testimonial-slider").owlCarousel({
+      items: 1,
+      loop: true,
+      autoplay: true,
+      autoplayTimeout: 5000,
+      autoplayHoverPause: true,
+      nav: false,
+      dots: true,
+    //   navText: [
+    //     "<i class='fa fa-chevron-left'></i>",
+    //     "<i class='fa fa-chevron-right'></i>"
+    //   ]
+    });
+  });
