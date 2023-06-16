00723f41498b06d51279b47c41f5aca1001d2be1
usmanshan39 <70626124+usmanshan39@users.noreply.github.com>
2023-06-16 12:27:27 +0500

    add db

--- admin/database/creation31.sql
+++ admin/database/creation31.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 29, 2023 at 02:46 PM
+-- Generation Time: Jun 16, 2023 at 09:26 AM
 -- Server version: 10.4.25-MariaDB
 -- PHP Version: 8.1.10
 
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `ygsaloon`
+-- Database: `creation31`
 --
 
 -- --------------------------------------------------------
@@ -34,9 +34,19 @@ CREATE TABLE `appointments` (
   `mobile` varchar(255) NOT NULL,
   `app_date` varchar(255) NOT NULL,
   `app_time` varchar(255) NOT NULL,
+  `service_type` varchar(255) NOT NULL,
   `status` varchar(255) NOT NULL DEFAULT 'Pending',
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `appointments`
+--
+
+INSERT INTO `appointments` (`id`, `name`, `email`, `mobile`, `app_date`, `app_time`, `service_type`, `status`, `updated_at`) VALUES
+(1, 'Mohsin', 'mohsin@gmail.com', '123454321123', '2023-06-05', '12:50', 'hair cut', 'Complete', '2023-06-16 07:05:20'),
+(2, 'home', 'home1@gmail.com', '2312323232', '2023-06-14', '12:22', 'nails', 'Complete', '2023-06-16 07:24:46'),
+(3, 'modal', 'modal@gmail.com', '23321232', '2023-06-26', '12:29', 'mackup', 'Pending', '2023-06-16 07:24:18');
 
 -- --------------------------------------------------------
 
@@ -54,6 +64,13 @@ CREATE TABLE `our_blogs` (
   `meta_desc` longtext NOT NULL,
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `our_blogs`
+--
+
+INSERT INTO `our_blogs` (`id`, `title`, `blog_image`, `blog_desc`, `published`, `meta_title`, `meta_desc`, `updated_at`) VALUES
+(1, 'one', '../uploads/foot-spa.jpg', 'What is Lorem Ipsum?\r\nLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.', 1, 'home-12', 'meta-desc', '2023-06-07 07:49:58');
 
 -- --------------------------------------------------------
 
@@ -108,13 +125,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `appointments`
 --
 ALTER TABLE `appointments`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `our_blogs`
 --
 ALTER TABLE `our_blogs`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `users`
